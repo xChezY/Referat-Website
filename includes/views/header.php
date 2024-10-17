cc143f0e0f519549f4ee66ca9c6135cede05e017
Doouni <69361991+Dounii@users.noreply.github.com>
2024-10-17 15:16:16 +0200

    Update header.php

---
+++ includes/views/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #344a3c;
+        }
+        
+        li {
+          float: left;
+        }
+        
+        li a {
+          display: inline-block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+        
+        li a:hover {
+          background-color: black;
+        }
+        </style>
+        </head>
+        <body>
+        <div style="font-size: 4em;">
+        <ul style="text-decoration: underline solid black;">
+            <li><img src="https://hapy-vernetzt.de/cms/wp-content/uploads/2023/10/Regional-vernetzen-1.png" width="93" height="93"></li>
+         <div style="float: right;">
+          <li style="border: groove green;"><a href="#home"><u>Home</u></a></li>
+          <li style="border: groove green;"><a href="#Pflanze"><u>Pflanze</u></a></li>
+          <li style="border: groove green;"><a href="#Kontakt"><u>Kontakt</u></a></li>
+         </div>
+        </ul>
+        <div>
+        </body>
+        
+</html>
