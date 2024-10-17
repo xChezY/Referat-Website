4c268f14bdeaf8f7d92286d36ad97370e13394c6
Doouni <69361991+Dounii@users.noreply.github.com>
2024-10-17 15:18:05 +0200

    Update footer.php

---
+++ includes/views/footer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+    <style>
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #333;
+        }
+        
+        li {
+          float: left;
+        }
+        
+        li a {
+          display: inline-block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+        
+        li a:hover {
+          background-color: #111;
+        }
+        </style>
+</head>
+<body>
+<footer>
+    <ul>
+        <li><img src="https://hapy-vernetzt.de/cms/wp-content/uploads/2023/10/Regional-vernetzen-1.png" width="100" height="100"></li>
+
+            <li><a href="mailto:Carlo.niermann@iu-study.org">Carlo.niermann@iu-study.org</a></li>
+            <li><a>Tel.1234567890</a></li>
+            <br>
+            <li><a href="mailto:bui-anh-minh-leon.phan@iu-study.org">bui-anh-minh-leon.phan@iu-study.org</a></li>
+            <li><a>Tel.1234567890</a></li>
+      
+           </div>
+           <div>
+            <li><a>Firma...</a></li> <div style="float: right;">
+
+       <br>
+       <li><a>Straße...</a></li>
+       <br>
+       <li><a>Zip+Stadt...</a></li>
+       <br>
+        <li><a href="#Impressum"><u>Impressum</u></a></li>  
+       <li><a>|</a></li>
+       <li><a href="#Datenschutz"><u>Datenschutz</u></a></li>  
+            </div>  
+     
+    </ul>
+</footer>
+</body>
+</html>
