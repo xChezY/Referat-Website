6fb69e80f8558ceafabf5d090052d586ee550c88
Doounii <Carlo.zuhaus@icloud.com>
2024-11-24 23:32:14 +0100

    Impressum 1

---
+++ contact/impressum.php
@@ -0,0 +1,15 @@
+<?php
+echo <<<HTML
+<div style="text-align: block; max-width: 1400px; margin: 1 auto;">
+        <p>
+        Bui Anh Minh Leon Phan<br>
+        Email: <a href="mailto:bui-anh-minh-leon.phan@iu-study.org">bui-anh-minh-leon.phan@iu-study.org</a>
+    </p>
+    <br>
+    <p>
+    Carlo Niermann<br>
+      Email: <a href="mailto:Carlo.Niermann@iu-study.org">Carlo.Niermann@iu-study.org</a>
+    </p>
+</div>
+HTML;
+?>
