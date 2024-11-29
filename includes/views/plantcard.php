7ed92532daea6875ce209e78a30a5669429d4cb4
xChezY <it@leonphan.dev>
2024-11-29 11:31:14 +0000

    add plants

--- includes/views/plantcard.php
+++ includes/views/plantcard.php
@@ -1,7 +1,7 @@
 <a href="#" id=<?php echo $id ?>>
     <div class="mt-4 flex flex-col items-center bg-white border border-gray-200 rounded-xl shadow lg:flex-row lg:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
         <img class="rounded-full object-cover h-36 w-36 p-4"
-            src="https://www.olerum.de/content/files/41316/Cucurbita-pepo-930x600-proportionalsmallest.webp"
+            src="<?php echo '../assets/plants/' . $name . ".jpg"; ?>"
             alt="">
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo $name; ?>
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html class="scroll-smooth" lang="en">
 <?php
+
+include "vendor/autoload.php";
+$db = new Referat\Database("database.db");
+$plant = $db->getThreePlants()[0];
+
 $title = "Plant Monitor";
 include "includes/views/head.php";
 ?>
@@ -16,9 +21,11 @@ include "includes/views/head.php";
                 <h1 class="text-6xl font-semibold">PlantMonitor</h1>
                 <p class="font-light text-3xl mt-5">The land of opportunity.</p>
                 <a href="#about" class="inline-flex items-center justify-center px-5 py-3 mr-3 mt-10 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
+                    Get started
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </a>
             </div>
         </div>
     </section>
@@ -51,12 +58,39 @@ include "includes/views/head.php";
                 <a href="/plants"
                     class="mt-8 flex flex-col items-center bg-white border border-gray-200 rounded-xl shadow lg:flex-row lg:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <img class="rounded-full object-cover h-48 w-48 p-4"
-                        src="https://www.olerum.de/content/files/41316/Cucurbita-pepo-930x600-proportionalsmallest.webp"
+                        src="assets/plants/<?php echo $plant["name"]; ?>.jpg"
                         alt="">
                     <div class="flex flex-col justify-between p-4 leading-normal">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sprössling</h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
-                            technology acquisitions of 2021 so far.</p>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo $plant["name"]; ?>
+                        </h5>
+                        <div class="grid grid-row-2">
+                            <div class="row-start-1 flex items-center">
+                                <p class="font-normal text-gray-700 dark:text-gray-40 mr-2">Temperatur:</p>
+                                <?php
+
+                                function getPlantStatus($current, $min, $max)
+                                {
+                                    if ($current < $min) {
+                                        return "red";
+                                    } elseif ($current > $max) {
+                                        return "purple";
+                                    } else {
+                                        return "primary";
+                                    }
+                                }
+
+                                $status = getPlantStatus($plant["current_temperature"], $plant["min_temperature"], $plant["max_temperature"]);
+                                include "includes/views/status.php"
+                                ?>
+                            </div>
+                            <div class="row-start-2 flex items-center">
+                                <p class="font-normal text-gray-700 dark:text-gray-400 mr-2">Bodenfeuchtigkeit:</p>
+                                <?php
+                                $status = getPlantStatus($plant["current_water"], $plant["min_water"], $plant["max_water"]);
+                                include "includes/views/status.php"
+                                ?>
+                            </div>
+                        </div>
                     </div>
                 </a>
 
